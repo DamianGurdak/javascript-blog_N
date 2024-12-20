1b00178420fa2d37a9d80334596c47ef348b52d6
Damian Gurdak <damiangurdakk@gmail.com>
2024-12-20 21:28:05 +0100

    add function generateTitleLinks

--- js/script.js
+++ js/script.js
@@ -40,34 +40,50 @@
     targetArticle.classList.add('active');
   };
 
-  const links = document.querySelectorAll('.titles a');
-
-  for (let link of links) {
-    link.addEventListener('click', titleClickHandler);
-  }
-
   const ArticleSelector = '.post',
     TitleSelector = '.post-title',
     TitleListSelector = '.titles';
 
   const generateTitleLinks = function () {
-    console.log('Function generateTitleLinks is wroking');
-
     /* remove contents of titleList */
     const titleList = document.querySelector(TitleListSelector);
     titleList.innerHTML = '';
 
     /* for each article */
+    const articles = document.querySelectorAll(ArticleSelector);
 
+    let html = '';
+
+    for (let article of articles) {
       /* get the article id */
+      const articleId = article.getAttribute('id');
 
       /* find the title element */
-
       /* get the title from the title element */
+      const articleTitle = article.querySelector(TitleSelector).innerHTML;
+      // console.log(articleTitle);
 
       /* create HTML of the link */
+      const linkHTML =
+        '<li><a href="#' +
+        articleId +
+        '"><span>' +
+        articleTitle +
+        '</span></a></li>';
+      // console.log(linkHTML);
 
       /* insert link into titleList */
+      html = html + linkHTML;
+      // console.log(html);
+    }
+
+    titleList.innerHTML = html;
+
+    const links = document.querySelectorAll('.titles a');
+    // console.log(links);
+    for (let link of links) {
+      link.addEventListener('click', titleClickHandler);
+    }
   };
 
   generateTitleLinks();
