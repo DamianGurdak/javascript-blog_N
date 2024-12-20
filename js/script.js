c681a90752c9c4afd58ac6ebfde18c3fccc15fe7
Damian Gurdak <damiangurdakk@gmail.com>
2024-12-20 17:05:53 +0100

    remove class active from articles

--- js/script.js
+++ js/script.js
@@ -5,10 +5,23 @@ const titleClickHandler = function (event) {
   console.log(event);
 
   /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a.active');
+  console.log('activeLinks: ', activeLinks);
+
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove('active');
+  }
 
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
+
+  const activeArticles = document.querySelectorAll('.posts article.active');
+  console.log('activeArticles: ', activeArticles);
+
+  for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove('active');
+  }
 
   /* get 'href' attribute from the clicked link */
 
